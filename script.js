bfbd60da05ecedd373d7f0821188f8db6a16585a
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-02-20 09:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+		let s = str.toLowerCase();
+	    let arr = [...s];  
+	    let arrReverse = [...s].reverse();
+	
+	    for (let i = 0; i < arr.length; i++) {
+	        if (arr[i] != arrReverse[i]) {
+	            return false;
+	        }
+	    }
+		return true;
 }
 module.exports = palindrome
